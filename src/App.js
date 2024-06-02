2a446f720cf8b5a87a8f6ac990e214658a3a4e5c
sowmya <r190157@rguktrkv.ac.in>
2024-06-02 19:35:47 +0530

    I have created a stylesheet

--- src/App.js
+++ src/App.js
@@ -5,25 +5,29 @@ import { BrowserRouter,Routes,Route }from "react-router-dom"
 import Contact from "./Contact.js"
 import Login from "./Login.js"
 import About from "./About.js"
+import Logout from "./Logout.js"
 
 
 
+      
 function App() {
   return (
-    
-   <>
-    
+   
+  <>
       <BrowserRouter>
-        <h2>App component</h2>
+        <div className="bg-container">
+        <h2 className="body">App component</h2>
          <Routes>
           <Route path="/"element={<Home/>}/>
           <Route path="/about"element={<About/>}/>
           <Route path="/contact"element={<Contact/>}/>
           <Route path="/login"element={<Login/>}/>
+          <Route path ="/logout"element={<Logout/>}/>
          </Routes>
+         </div>
       </BrowserRouter>
-  
-    </>     
+  </>
+      
   );
 }
 
---
+++ src/Logout.js
@@ -0,0 +1,6 @@
+function Logout(){
+    return(
+        <h1>This is Logout component</h1>
+    )
+}
+export default Logout
