5b11bdd1e1ba10ffe1e26c05753e0a707e1f3dee
sowmya <r190157@rguktrkv.ac.in>
2024-06-03 10:01:40 +0530

    created  a new component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contact from "./Contact.js"
 import Login from "./Login.js"
 import About from "./About.js"
 import Logged from "./Logged.js"
-
+import Calculator from "./Calculator.js"
 
 
       
@@ -16,13 +16,14 @@ function App() {
   <>
       <BrowserRouter>
         <div className="bg-container">
-        <h2 className="body">App component</h2>
+        <h2 className="body">Calculator component</h2>
          <Routes>
           <Route path="/"element={<Home/>}/>
           <Route path="/about"element={<About/>}/>
           <Route path="/contact"element={<Contact/>}/>
           <Route path="/login"element={<Login/>}/>
           <Route path ="/logged"element={<Logged/>}/>
+          <Route path ="/Calculator"element={<Calculator/>}/>
          </Routes>
          </div>
       </BrowserRouter>
---
+++ src/Calculator.js
@@ -0,0 +1,6 @@
+function Calculator(){
+    return(
+        <h1> this is calculator component</h1>
+    )
+}
+export default Calculator
